ab6102486f5c61035121b5c305b86ff07688480f
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-08-09 19:24:30 +0300

    add models and migrations for books and categories

---
+++ database/migrations/2025_08_09_161638_create_book_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Book;
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Book::class)->constrained()->onDelete('cascade');
+            $table->boolean('is_returned')->default(false);
+            $table->timestamp('borrow_date')->nullable();
+            $table->timestamp('return_date')->nullable();
+            $table->unique(['user_id', 'book_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book_user');
+    }
+};
