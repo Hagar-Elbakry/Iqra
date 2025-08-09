e43ca0b272c98d876a8997d2bbc07c3311ee8d91
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-08-09 22:30:55 +0300

    fix merge issuses

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\HomeController;
-=======
 use App\Http\Controllers\ProfileController;
->>>>>>> 090b6dd4ebf4ed3876f5ae03f76d2d9b0bd38515
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
