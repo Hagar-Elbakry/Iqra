d4081269db89b3690705a84a865433f504be98fb
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-08-10 15:33:38 +0300

    add table layout in dashboard, enhance button styles in profile forms

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,27 @@
 <x-app>
     <x-nav/>
+    <div style="padding: 50px">
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col" style="padding-left: 10px;">Book</th>
+            <th scope="col">Borrow Date</th>
+            <th scope="col">Return Date</th>
+            <th scope="col">Is Returned</th>
+            <th scope="col" style="padding-left: 30px">Return</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td style="padding-left: 10px;">divergent</td>
+            <td>{{now()}}</td>
+            <td>{{now()}}}</td>
+            <td>no</td>
+            <td><x-primary-button class="ms-4" style="height: 40px;">
+                {{ __('Return') }}
+                </x-primary-button></td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
 </x-app>
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -48,7 +48,7 @@
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button style="height: 40px">{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'profile-updated')
                 <p
